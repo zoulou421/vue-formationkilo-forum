4ab19e2d2599add6aa23107912cddbc186e890d5
Bonevy BEBY <bonevybeby@gmail.com>
2024-03-24 17:24:13 +0100

    added: display data and in particular: thread title

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,9 +1,20 @@
+<!-- eslint-disable vue/require-v-for-key -->
 <template>
- Hello from  PageHome
+ <div>Hello from  PageHome</div>
+ <div v-for="thread in threads" :key="thread.id">
+  <h1>{{thread.title}}</h1>
+</div>
 </template>
 
 <script>
-
+import sourceData from '@/data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads
+    }
+  }
+}
 </script>
 
 <style scoped>
