fc09d77f947997edb0c6b7e89eae48613ba4e774
Bonevy BEBY <bonevybeby@gmail.com>
2024-03-24 21:36:42 +0100

    added:style our components to increase the performance of our vue.js applications

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,14 @@ export default {
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
+@import 'assets/style.css'
+/*@import '~bootstrapp/css/bootstrapp.css' to import bootstrap or materialize*/
 </style>
