8bcb4a2cc032b9fdfac37bcdf20fee3181600fdf
Bonevy BEBY <bonevybeby@gmail.com>
2024-03-24 11:21:01 +0100

    added: display data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,14 +27,26 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>
+      {{threads}}
+    </div>
   </div>
 </template>
 
 <script>
+import sourceData from '@/data.json'
+console.log(sourceData.threads)
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      // sourceData: sourceData
+      threads: sourceData.threads
+    }
   }
 }
 </script>
